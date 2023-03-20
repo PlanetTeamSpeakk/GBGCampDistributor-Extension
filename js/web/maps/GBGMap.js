6f093e3072b41feb063b09c9a60bfb43565f8fd7
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-03-20 19:37:42 +0100

    They are

--- js/web/maps/GBGMap.js
+++ js/web/maps/GBGMap.js
@@ -11,8 +11,6 @@ class GBGMap {
     /**
      * Converts a province id to its name.
      * This is primarily possible because province names follow a pretty strict pattern.
-     * Whether the names of provinces are the same every season (assuming the same map) is unknown,
-     * but I am pretty certain they are as the server does not inform the client of province names.
      * @param id {number} The id to convert
      * @return {string} The name of the province associated with the given id
      */
